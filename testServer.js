a0fb1dd36f5117035740db5d001ba9a709912482
Ryan Scovill <ryans797@gmail.com>
2017-03-19 07:29:10 -0700

    remove unecessary files

--- testServer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(42069, 'localhost');
-console.log('Server running at http://localhost:42069/');
